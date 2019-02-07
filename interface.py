008b064172d01639f638830ede666607a2eedb28
Mykola Gavrysh <mgavrysh1@sheffield.ac.uk>
2019-02-07 19:33:02 +0000

    Cleaned the code

--- interface.py
+++ interface.py
@@ -4,6 +4,7 @@ from PyQt5.QtCore import QTimer
 from PyQt5.uic import loadUi
 from PyQt5.QtGui import QPixmap, QImage
 import cv2
+import serial_com
 
 
 
@@ -11,14 +12,15 @@ import cv2
 class Webcome(QDialog):
     screen = " ";
     screen1 = " ";
-    #caption = " ";
-
 
     def __init__(self):
-        self.camera = 1;
+        self.noob = "";
+        self.camera = 0;
         super(Webcome,self).__init__()
         loadUi('webcamera.ui',self)
         self.image = None
+
+        self.Arduino.clicked.connect(self.Arduino1)
         self.startButton.clicked.connect(self.start_webcam)
         self.endButton.clicked.connect(self.stop_webcam)
         self.Front.clicked.connect(self.FrontA)
@@ -26,23 +28,20 @@ class Webcome(QDialog):
         self.Back.clicked.connect(self.BackA)
         self.Back_2.clicked.connect(self.BackB)
        # self.Left.clicked.connect(self.LeftA)
-       # self.Left_2.clicked.connect(self.LeftB)
-       # self.Right.clicked.connect(self.RightA)
-       # self.Right_2.clicked.connect(self.RightB)
+    #        # self.Left_2.clicked.connect(self.LeftB)
+    #        # self.Right.clicked.connect(self.RightA)
+    #        # self.Right_2.clicked.connect(self.RightB)
 
     def start_webcam(self):
         self.capture = cv2.VideoCapture(self.camera)
-        #self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT,1080)#500
-        #self.capture.set(cv2.CAP_PROP_FRAME_WIDTH,1920)#900
 
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.update_frame)
-        self.timer.start(5)
+        self.timer.start(6)
 
     def update_frame(self):
-        ret,self.image = self.capture.read()  #takes Video capture
-        #self.image = cv2.flip(self.image, 1)
-        #both_img = cv2.flip(self.image, -1)
+        ret,self.image = self.capture.read()
+        self.image = cv2.flip(self.image, 1)
         self.displayImage(self.image)
 
 
@@ -51,21 +50,20 @@ class Webcome(QDialog):
 
     def displayImage(self,img,window=1):
         qformat = QImage.Format_Indexed8
-        if len(img.shape)== 3 : #[0] - rows,  [1] = columns , [2] - chanel less
+        if len(img.shape)== 3 :
           if img.shape[2]== 4 :
             qformat = QImage.Format_RGBA8888
           else:
             qformat = QImage.Format_RGB888
 
         outImage = QImage(img,img.shape[1],img.shape[0],img.strides[0],qformat)
-        #BGR>>RGB
         outImage = outImage.rgbSwapped()
 
         if window == 1:
          self.MainDisplay.setPixmap(QPixmap.fromImage(outImage))
          self.MainDisplay.setScaledContents(True)
-         self.MainDisplay_2.setPixmap(QPixmap.fromImage(outImage))
-         self.MainDisplay_2.setScaledContents(True)
+         #self.MainDisplay_2.setPixmap(QPixmap.fromImage(outImage))
+         #self.MainDisplay_2.setScaledContents(True)
 
     def FrontA (self):
         self.ChangeScreenF()
@@ -101,9 +99,10 @@ class Webcome(QDialog):
 
 
     def ChangeScreenF (self):
-        self.camera = 2;
+        self.camera = 0;
         global screen
         screen = "Front"
+        self.timer.stop()
         self.start_webcam()
 
     def ChangeScreenF1 (self):
@@ -120,33 +119,7 @@ class Webcome(QDialog):
         global screen1
         screen1 = "Back"
 
-
-
-
-
-
-
-
-
-
-
-
-
-   # def LeftA (self):
-
-
-  #  def LeftB (self):
-
-
-  #  def RightA (self):
-
-
-   # def RightB (self):
-
-        #self.Front_2.hide()
-       # self.Right.show()
-       # self.Left.show()
-       # self.Back.show()
+    def Arduino1(self):
 
 
 
@@ -155,5 +128,4 @@ if __name__ == '__main__':
     window = Webcome()
     window.setWindowTitle("Avalon Graphical Interface");
     window.show()
-    window.showFullScreen()
     sys.exit(app.exec_())
