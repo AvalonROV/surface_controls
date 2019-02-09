df55e3fb7ac7351b508bf6fac1c6ea7afdf671dd
Marwan99 <marokhaled99@gmail.com>
2019-02-09 15:07:07 +0000

    Added video class & mainwindow

--- interface.py
+++ interface.py
@@ -1,70 +1,97 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QDialog
-from PyQt5.QtCore import QTimer
+from PyQt5.QtWidgets import QMainWindow, QApplication, QDialog
+from PyQt5.QtCore import QTimer, QThread
 from PyQt5.uic import loadUi
-from PyQt5.QtGui import QPixmap, QImage
+from PyQt5.QtGui import QPixmap, QImage   
 import cv2
 import serial_com
 
-
-
-
-class Webcome(QDialog):
-    screen = " ";
-    screen1 = " ";
-
+class get_video_feed(QThread):
+    def __init__(self, channel):
+        QThread.__init__(self)
+        self.channel = channel
+               
+        
+    def get_new_frame(self):
+        ret, frame = self.capture.read()
+        
+        if ret:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            #frame = cv2.flip(frame, 1)
+            image = QImage(frame, frame.shape[1], frame.shape[0], 
+                           frame.strides[0], QImage.Format_RGB888)
+        else:
+            image = None
+        return ret, image
+    
+    def run(self):
+        self.capture = cv2.VideoCapture(self.channel) 
+        #self.get_new_frame()
+    
+    def __del__(self):
+        self.wait()
+    
+    def end_feed(self):
+        self.capture.release()
+        print("Camera feed on channel " + str(self.channel) + " closed")
+    
+class Window(QMainWindow):
     def __init__(self):
-        self.noob = "";
-        self.camera = 0;
-        super(Webcome,self).__init__()
+        super(Window,self).__init__()
         loadUi('webcamera.ui',self)
-        self.image = None
-
-        self.Arduino.clicked.connect(self.Arduino1)
-        self.startButton.clicked.connect(self.start_webcam)
-        self.endButton.clicked.connect(self.stop_webcam)
+        self.setWindowTitle("Avalon Graphical Interface");
+        '''
         self.Front.clicked.connect(self.FrontA)
         self.Front_2.clicked.connect(self.FrontB)
         self.Back.clicked.connect(self.BackA)
         self.Back_2.clicked.connect(self.BackB)
-       # self.Left.clicked.connect(self.LeftA)
-    #        # self.Left_2.clicked.connect(self.LeftB)
-    #        # self.Right.clicked.connect(self.RightA)
-    #        # self.Right_2.clicked.connect(self.RightB)
+        #self.Arduino.clicked.connect(self.Arduino1)
+        '''
+        
+        #self.comms = serial_com.SerialComms('COM6')        
+        
+        self.show()
+        self.display_feed(1, 2)
+        
+    def closeEvent(self, event):
+        print( "Exiting application...")
+        self.timer.stop()
+        self.video_1.end_feed()
+        self.video_2.end_feed()
+        #self.comms.end_comms()
+        event.accept()
+        #sys.exit()
 
-    def start_webcam(self):
-        self.capture = cv2.VideoCapture(self.camera)
-
+    def display_feed(self, channel_1, channel_2):
+        self.video_1 = get_video_feed(channel_1)
+        self.video_1.start()
+        
+        self.video_2 = get_video_feed(channel_2)
+        self.video_2.start()
+        
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.update_frame)
-        self.timer.start(6)
-
+        self.timer.start(50)
+        
     def update_frame(self):
-        ret,self.image = self.capture.read()
-        self.image = cv2.flip(self.image, 1)
-        self.displayImage(self.image)
-
-
-    def stop_webcam(self):
-        self.timer.stop()
-
-    def displayImage(self,img,window=1):
-        qformat = QImage.Format_Indexed8
-        if len(img.shape)== 3 :
-          if img.shape[2]== 4 :
-            qformat = QImage.Format_RGBA8888
-          else:
-            qformat = QImage.Format_RGB888
-
-        outImage = QImage(img,img.shape[1],img.shape[0],img.strides[0],qformat)
-        outImage = outImage.rgbSwapped()
-
-        if window == 1:
-         self.MainDisplay.setPixmap(QPixmap.fromImage(outImage))
-         self.MainDisplay.setScaledContents(True)
-         #self.MainDisplay_2.setPixmap(QPixmap.fromImage(outImage))
-         #self.MainDisplay_2.setScaledContents(True)
-
+        try:
+            state, image = self.video_1.get_new_frame()
+            if state == True:
+                self.MainDisplay.setPixmap(QPixmap.fromImage(image))
+                self.MainDisplay.setScaledContents(True)
+            else:
+                print("No feed avaliable for Display 1")
+            
+            state, image = self.video_2.get_new_frame()
+            if state == True:
+                self.MainDisplay_2.setPixmap(QPixmap.fromImage(image))
+                self.MainDisplay_2.setScaledContents(True)
+            else:
+                print("No feed avaliable for Display 2")
+        except:
+            pass
+        
+    '''    
     def FrontA (self):
         self.ChangeScreenF()
         if "Front" == screen :
@@ -97,7 +124,6 @@ class Webcome(QDialog):
             self.Front.show()
             self.Front_2.show()
 
-
     def ChangeScreenF (self):
         self.camera = 0;
         global screen
@@ -118,14 +144,9 @@ class Webcome(QDialog):
     def ChangeScreenB1 (self):
         global screen1
         screen1 = "Back"
-
-    def Arduino1(self):
-
-
-
+    '''
+    
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = Webcome()
-    window.setWindowTitle("Avalon Graphical Interface");
-    window.show()
+    GUI_window = Window()
     sys.exit(app.exec_())
