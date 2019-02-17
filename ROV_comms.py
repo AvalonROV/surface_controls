3497f4e104dd8ca9c350f6443368535aaf69fe38
Marwan99 <marokhaled99@gmail.com>
2019-02-17 11:54:38 +0000

    Added testing function

--- ROV_comms.py
+++ ROV_comms.py
@@ -1,7 +1,6 @@
 import serial
-import serial.tools.list_ports as ls_port
+import serial.tools.list_ports
 from PyQt5.QtCore import QThread, pyqtSignal
-import sys 
 
 class ls_COM_ports(QThread):
     signal = pyqtSignal(list)
@@ -21,17 +20,19 @@ class ls_COM_ports(QThread):
             
         print("Port listing thread terminating")
 
-class Serial:
+class Serial: 
     """
     port  --> COM(X) for windows systems, dev/tty/USB(X) for linux based system
     baud_rate ---> {9600, 152000.. etc}
+    All messages sent from the ROV should be terminated with '\n'
     """
     def __init__(self, baud_rate = 115200):
-        self.ser = serial.Serial()
+        self.ser = serial.Serial(timeout=1)
         self.ser.baudrate = baud_rate
     
     def update_port(self, new_port):
         self.ser.port = new_port
+        self.ser.open()
         print("Serial port changed to:" + new_port)
     
     def is_open(self):
@@ -46,9 +47,21 @@ class Serial:
     
     def get_telemetry(self):
         self.ser.write('GT\n'.encode('ascii'))
-        data = self.ser.readline().strip().decode('ascii')
-        return data
-    
+        data = self.ser.readline().strip().decode('ascii').split(',')
+        return {
+                "depth" : data[1] + "m",
+                "temprature" : data[2] + "C",
+                "ph" : data[3],
+                "pitch_angle" : data[4] + "deg",
+                "roll_angle" : data[5] + "deg"
+                }
+        '''return {
+                "depth" : "1.23m",
+                "temprature" : "21.64C",
+                "ph" : "7.15",
+                "pitch_angle" : "0deg",
+                "roll_angle" : "0deg"
+                }'''
     def set_thrsuters(self, power):
         payload = 'ST\n'
         for value in power:
@@ -86,13 +99,15 @@ class Serial:
     def set_depth_calibration(self, p, i, d):
         payload = 'SD_trim\n'
         self.ser.write(payload.encode('ascii'))
+    
+    def testing_function(self, payload):
+        payload += "\n"
+        self.ser.write(payload.encode('ascii'))
+        data = self.ser.readline().strip().decode('ascii')
+        print(data)
         
 if __name__ == "__main__":
-    comms = SerialComms('COM6')
+    comms = Serial()
+    comms.update_port('COM7')
     while(1):
-        try:
-            print(comms.getPotentiometerValues())
-        except:
-            comms.end_comms()
-            print("Port cLosed!")
-            sys.exit()
+        comms.testing_function("Hello World!")
