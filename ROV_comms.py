e4a8eb4a5d7767573bbd692ed1f7c0a52c54bfca
Marwan99 <marokhaled99@gmail.com>
2019-06-09 21:27:08 +0100

    Code refactored, thread added

--- ROV_comms.py
+++ ROV_comms.py
@@ -19,18 +19,35 @@ class ls_COM_ports(QThread):
                 self.old_list = new_list
                 print(self.old_list)
             
+#            time.sleep(0.25)
+            
         print("Port listing thread terminating")
 
-class Serial: 
+class Serial(QThread): 
     """
     port  --> COM(X) for windows systems, dev/tty/USB(X) for linux based system
     baud_rate ---> {9600, 152000.. etc}
     All messages sent from the ROV should be terminated with '\n'
     """
-    def __init__(self, baud_rate = 115200):
+    signal = pyqtSignal(str)
+    
+    telemetry = {
+            "temp" : "N/A",
+            "pH" : "N/A",
+            "humidity" : "N/A",
+            "depth" : "N/A",
+            "roll" : "N/A",
+            "pitch" : "N/A"
+            }
+    
+    def __init__(self, baud_rate = 115200, parent=None):
+        QThread.__init__(self, parent)
         self.ser = serial.Serial(timeout=0.1)
         self.ser.baudrate = baud_rate
-    
+        
+        self.running = True
+        self.Tx_payload_queue = []
+        
     def update_port(self, new_port):
         self.ser.port = new_port
         self.ser.open()
@@ -38,83 +55,107 @@ class Serial:
     
     def is_open(self):
         return self.seris_open
-        
-    def end_comms(self):
-        self.ser.close()
-        print("Serial port closed")
     
-    def get_telemetry(self): # Needs finishing
-        self.ser.write('SK\n'.encode('ascii'))
-        data = self.ser.readline().strip().decode('ascii').split(',')
-        if data[0] == "":
-            return{
-                "depth" : "N/A",
-                "temprature" : "N/A",
-                "ph" : "N/A",
-                "pitch_angle" : "N/A",
-                "roll_angle" : "N/A"
-                }
-        else:
-            return {
-                    "depth" : data[1] + "m",
-                    "temprature" : data[2] + "C",
-                    "ph" : data[3],
-                    "pitch_angle" : data[4] + "deg",
-                    "roll_angle" : data[5] + "deg"
-                    }
+    def run(self):
+        
+        while self.running:
+            #--Rx-------------------------------------------------------------#
+            data = self.ser.readline().strip().decode('ascii')
+#            print(data)
+            try:
+                if data[0] == "R":
+                    if data[1] == "A": # Temprature
+                        self.telemetry["temp"] = str(data[2:])
+                    
+                    elif data[1] == "B": # humidity:
+                        self.telemetry["pH"] = str(data[2:])
+                    
+                    elif data[1] == "C": # roll angle
+                        self.telemetry["depth"] = str(data[2:])
+                    
+                    elif data[1] == "D": # pitch angle
+                        self.telemetry["roll"] = str(data[2:])
+                        
+                    elif data[1] == "E": # pitch angle
+                        self.telemetry["pitch"] = str(data[2:])
+                    
+                    elif data[1] == "F": # pitch angle
+                        self.telemetry["humidity"] = str(data[2:])
+                    
+                    else:
+                         self.signal.emit(">> Error: undefined message payload.")
+                         
+                else:
+                    self.signal.emit(">> Error: missing \"R\" at the start of the message.")
+             
+                self.signal.emit(">> " + data)
+                
+            except Exception as e:
+                self.signal.emit("ERROR: Issue with data received:")
+                self.signal.emit(str(e))
+            
+            #--Tx-------------------------------------------------------------#
+            if len(self.Tx_payload_queue):
+                data = self.Tx_payload_queue.pop(0)
+                self.ser.write(data.encode('ascii'))
+                self.signal.emit("<< " + data)
+            
+#            time.sleep(0.03)
+                
+    def stop(self):
+        self.running = False
+        self.ser.close()
+        print("Serial port closed.")
         
     def set_thrsuters(self, power):
-        payload = 'SA' + power + '!'
-        self.ser.write(payload.encode('ascii'))
-        return payload
-    
-    def set_camera(self, channel_1, channel_2):
-        payload = 'SH' + channel_1 + channel_2 + '\n'
-        self.ser.write(payload.encode('ascii'))
-    
-    def set_gripper(self):
-        payload = 'SF\n'
-        self.ser.write(payload.encode('ascii'))
-    
-    def set_trap_door(self):
-        payload = 'SG\n'
-        self.ser.write(payload.encode('ascii'))
-    
-    def set_pid_controller_state(self, controller, state):
-        if controller == "depth":
-            payload = 'SD' + str(state) + '!'
-        elif controller == "pitch":
-            payload = 'SE' + str(state) + '!'
-        self.ser.write(payload.encode('ascii'))
-        return payload
+        self.Tx_payload_queue.append('SA' + power + '!')
         
     def set_pid_controller_gains(self, controller, p, i, d):
         if controller == "depth":
             payload = 'SB'
+            
         elif controller == "pitch":
             payload = 'SC'
         
-        payload += str(p) + ',' + str(i) + ',' + str(d) + '!'
-        self.ser.write(payload.encode('ascii'))
-        return payload
+        self.Tx_payload_queue.append(payload + str(p) + ',' + str(i) + ',' + str(d) + '!')
     
+    def set_pid_controller_state(self, controller, state):
+        if controller == "depth":
+            self.Tx_payload_queue.append('SD' + str(state) + '!')
+        
+        elif controller == "pitch":
+            self.Tx_payload_queue.append('SE' + str(state) + '!')
+                
+    def set_gripper(self, state):
+        self.Tx_payload_queue.append("SF" + str(state) + "aa\n")
+        
+    def inflate_lift_bag(self, state):
+        self.Tx_payload_queue.append("SG" + str(state) + "a" + "!")
+    
+    def set_camera(self, channel_1, channel_2):
+        self.Tx_payload_queue.append('SH' + channel_1 + "," + channel_2 + '!')
+        
     def trim_depth_sensor(self):
-        payload = 'SI\n'
-        self.ser.write(payload.encode('ascii'))
-    
+        self.Tx_payload_queue.append('SI!')
+
     def trim_imu(self):
-        payload = 'SJ\n'
-        self.ser.write(payload.encode('ascii'))
+        self.Tx_payload_queue.append('SJ!')
+    
+    def get_telemetry(self):
+        return self.telemetry
     
     def testing_function(self, payload):
-        payload += "\n"
+        payload += "!"
+#        print(payload)
         self.ser.write(payload.encode('ascii'))
         data = self.ser.readline().strip().decode('ascii')
-        print(data)
+#        print(data)
         
 if __name__ == "__main__":
     comms = Serial()
-    comms.update_port('COM11')
+    comms.update_port('COM6')
+#    comms.start()
+ 
     while(1):
         comms.testing_function("SA500500500500500600!")
         time.sleep(0.1)
